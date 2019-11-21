ccb6e2ced169cc193d7ff51cbfda2b863a9ab382
chen-rongliang <chen-rongliang@qq.com>
2019-11-21 19:14:45 +0800

    change weather component

--- src/axios/path.js
+++ src/axios/path.js
@@ -4,5 +4,7 @@
 
 module.exports = {
     suggest: 'https://m.baidu.com/su',
-    weather: 'https://free-api.heweather.net/s6/weather/'
+    weather: 'https://free-api.heweather.net/s6/weather/',
+    weather_ic: 'https://cdn.heweather.com/img/plugin/190516/icon/c/',
+    
 }
--- src/components/WeatherBox.vue
+++ src/components/WeatherBox.vue
@@ -1,10 +1,46 @@
 <template>
     <div class="weather-box">
-        <a class="btn btn-weather" :style="radius" @click="toggleWeather(true)"><span class="celsius">23℃</span>天河</a>
+        <a class="btn btn-weather" :style="radius" @click="toggleWeather(true)"><span class="celsius">{{ now.tmp || '--' }}°</span>{{ basic.location || '--' }}</a>
         <transition name="fade-in">
             <div class="weather-card" v-if="status">
+                <div class="cover" @click="toggleWeather(false)"></div>
+                <div class="card">
+                    <div class="row">
+                        <div class="location pull-left">
+                            {{ basic.admin_area + basic.parent_city + (basic.parent_city == basic.location ? '' : basic.location) }}
+                            <span class="link" @click="setlocation(`请输入你的所在位置（留空则自动判断ip）`)">切换</span>
+                        </div>
+                        <div class="pull-right"><a class="link" href="https://heweather.com" target="_blank">@和风天气</a></div>
+                    </div>
+                    <div class="row">
+                        <div class="temp pull-left">
+                            <img class="ic-cond" :src="weatherIc(now.cond_code + getCond())">
+                            <div class="text">
+                                <div class="num">{{ now.tmp || '--' }}°</div>
+                                {{ now.cond_txt || '--' }}
+                            </div>
+                        </div>
+                        <div class="wind pull-righ">
+                            <span class="btn-brf">{{ lifestyle[0].brf || '--' }}</span>
+                            <div>{{ now.wind_dir + now.wind_sc + '级' }}</div>
+                            <div>相对湿度{{ now.hum }}</div>
+                        </div>
+                    </div>
+                    <div class="hr"></div>
+                    <div class="row">
+                        <div class="forecast">
+                            <div
+                                v-for="(item, idx) in daily_forecast"
+                                :key="idx"
+                                class="item">
+                                <div class="date">{{ idx == 0 ? '今天' : shortTime(item.date) }}</div>
+                                <img class="ic-cond" :src="weatherIc(item['cond_code_' + getCond()] + getCond())">
+                                <div class="tmp">{{ item.tmp_min }}°/{{ item.tmp_max }}°</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 <b class="close" @click="toggleWeather(false)">×</b>
-                
             </div>
         </transition>
     </div>
@@ -17,21 +53,52 @@ import { cookie } from '~/js/utils'
 export default {
     name: 'WeatherBox',
     data () {
+        let tmp = {}
         return {
             status: false,
             param: {
                 location: '',
                 key: '7f3c55309e6b466fa8ac4ac7c808f34d'
-            }
+            },
+            basic: tmp,
+            daily_forecast: [],
+            lifestyle: [tmp],
+            now: tmp,
+            update: tmp
         }
     },
     props: ['radius'],
     methods: {
+        getCond () {
+            let mark = new Date().getHours() > 18 ? 'n' : 'd'
+            return mark
+        },
+        weatherIc (code) {
+            return $.path.weather_ic + code + '.png'
+        },
+        shortTime (time) {  
+            return time.substr(5)
+        },
         toggleWeather (status) {
+
+            if(!this.now.tmp) {
+                alert('还没准备好~')
+                return
+            }
+
             this.status = status
         },
-        geolocation () {
+        setlocation (str) {
 
+                let local = prompt(str)
+                if(!local) local = 'auto_ip'
+
+                this.param.location = local
+                cookie.set('location', local)
+
+                this.getWeather()
+        },
+        geolocation () {
             
             return new Promise ((resolve, reject) => {
                 
@@ -61,15 +128,18 @@ export default {
         },
         getWeather () {
             $.get(`${ $.path.weather }`, this.param).then(({ HeWeather6 }) => {
-                let { basic, daily_forecast, now, status, update } = HeWeather6.pop()
+
+                let { basic, daily_forecast, lifestyle, now, status, update } = HeWeather6.pop()
+
 
                 if(status == 'ok') {
-                    // TODO 获取天气信息之后，显示到页面上
-                } else {
-
+                    
+                    this.basic = basic
+                    this.lifestyle = lifestyle
+                    this.daily_forecast = daily_forecast
+                    this.now = now
+                    this.update = update
                 }
-            }).catch(() => {
-
             })
         }
     },
@@ -84,13 +154,8 @@ export default {
 
                 // 未获取到浏览器提供的位置信息，转入手动设置
 
-                let local = prompt(`${err}请手动输入你的所在位置（留空则自动判断ip）`)
-                if(!local) local = 'auto_ip'
-
-                this.param.location = local
-                cookie.set('location', local)
-
-                this.getWeather()
+                this.setlocation(`${err}请手动输入你的所在位置（留空则自动判断ip）`)
+                
             })
     }
 }
@@ -105,7 +170,14 @@ export default {
             right: 0;
             bottom: 0;
             left: 0;
-            background-color: rgba($color: #000, $alpha: 0.8);
+            background-color: rgba($color: #000, $alpha: 0.6);
+            .cover {
+                top: 0;
+                right: 0;
+                bottom: 0;
+                left: 0;
+                position: absolute;
+            }
             .close {
                 display: block;
                 width: 40px;
@@ -121,14 +193,96 @@ export default {
                 border-radius: 0 0 20px 20px;
                 box-shadow: 0 0 4px #ccc;
                 color: #000;
-                background-color: #fff;
+                background-color: rgba($color: #fff, $alpha: 0.6);;
                 cursor: pointer;
             }
-            iframe {
+            .card {
                 width: 100%;
-                max-width: 750px;
-                margin: auto;
-                height: 100%;
+                max-width: 592px;
+                padding: 32px 16px 10px;
+                top: 0;
+                left: 50%;
+                transform: translate3d(-50%,0,0);
+                font-size: 16px;
+                box-shadow: 0 0 4px #ccc;
+                position: absolute;
+                background-color: #fff;
+                .hr {
+                    height: 1px;
+                    background-color: #f0f0f0;
+                }
+                .link {
+                    color: #f69
+                }
+                .text {
+                    display: inline-block;
+                    vertical-align: top;
+                }
+                .temp {
+                    margin-top: 24px;
+                    text-align: left;
+                    font-size: 12px;
+                    font-weight: 100;
+                    .ic-cond {
+                        width: 54px;
+                        height: auto;
+                        margin: auto 10px auto auto;
+                    }
+                    .num {
+                        margin: 4px auto 3px;
+                        font-size: 20px;
+                        font-weight: 300;
+                    }
+                }
+                .wind {
+                    line-height: 1.1;
+                    margin: 28px auto 24px;
+                    direction: rtl;
+                    text-align: right;
+                    font-size: 12px;
+                    .btn-brf {
+                        display: inline-block;
+                        padding: 2px 10px;
+                        margin-bottom: 4px;
+                        border-radius: 12px;
+                        color: #fff;
+                        background-color: rgb(255, 157, 30)
+                    }
+                }
+                .forecast {
+                    overflow-x: auto;
+                    margin: 24px auto 14px;
+                    font-size: 0;
+                    white-space: nowrap;
+                    .item {
+                        display: inline-block;
+                        width: 22.5%;
+                        .date {
+                            font-size: 14px;
+                        }
+                        .ic-cond {
+                            width: 24px;
+                            height: auto;
+                            margin: 12px auto;
+                        }
+                        .tmp {
+                            font-size: 13px;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    @media(min-width: 592px) {
+        .weather-box {
+            .weather-card {
+                .card {
+                    .forecast {
+                        .item {
+                            width: 80px;
+                        }
+                    }
+                }
             }
         }
     }
